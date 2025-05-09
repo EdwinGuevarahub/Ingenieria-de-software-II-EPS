d06375f6ee96bb5a0195a4fbc84b65f87b725240
Nicolás Sabogal <mew-four@hotmail.com>
2025-05-09 14:40:40 -0500

    refactor: agregar documentación a la clase ServicioMedicoListaSolicitud para mejorar la claridad del código

--- EPS/backend/src/main/java/com/eps/apexeps/request/ServicioMedicoListaSolicitud.java
+++ EPS/backend/src/main/java/com/eps/apexeps/request/ServicioMedicoListaSolicitud.java
@@ -2,6 +2,12 @@ package com.eps.apexeps.request;
 
 import lombok.Data;
 
+/**
+ * Clase que representa una solicitud de servicio médico para listar servicios médicos.
+ * Contiene información sobre el nombre o código CUPS del servicio médico a buscar,
+ * el tamaño de la página y el número de la página.
+ * @autor Nicolás Sabogal
+ */
 @Data
 public class ServicioMedicoListaSolicitud {
     
