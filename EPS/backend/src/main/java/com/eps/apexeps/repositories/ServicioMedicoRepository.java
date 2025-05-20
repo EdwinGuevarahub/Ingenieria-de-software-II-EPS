1031d3aa8a4e286b075b1cc7990a6150b58d54e3
Nicolás Sabogal <mew-four@hotmail.com>
2025-05-19 20:47:39 -0500

    refactor: Cambiar métodos de búsqueda en repositorios para devolver páginas en lugar de listas (Agenda, Consultorio, Médico y Servicio Médico)

--- EPS/backend/src/main/java/com/eps/apexeps/repositories/ServicioMedicoRepository.java
+++ EPS/backend/src/main/java/com/eps/apexeps/repositories/ServicioMedicoRepository.java
@@ -1,8 +1,8 @@
 package com.eps.apexeps.repositories;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -32,7 +32,7 @@ public interface ServicioMedicoRepository extends JpaRepository<ServicioMedico, 
             OR sm.cups LIKE %:cupsNombreLike%
             OR sm.nombre LIKE %:cupsNombreLike%
     """)
-    List<ServicioMedico> findAllFiltered(String cupsNombreLike, Pageable pageable);
+    Page<ServicioMedico> findAllFiltered(String cupsNombreLike, Pageable pageable);
 
     Optional<ServicioMedico> findByCups(String cups);
 
