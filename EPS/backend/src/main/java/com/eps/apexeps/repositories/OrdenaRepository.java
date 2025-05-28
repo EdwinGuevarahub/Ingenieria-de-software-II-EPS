a8ddb4bca6460b80dd6cd986eba9c0af213820e3
wfpinedar <wfpinedar@gmail.com>
2025-05-27 21:41:05 -0500

    {feat}: Add functionality for Solicitar Exámen

--- EPS/backend/src/main/java/com/eps/apexeps/repositories/OrdenaRepository.java
+++ EPS/backend/src/main/java/com/eps/apexeps/repositories/OrdenaRepository.java
@@ -1,11 +1,27 @@
 package com.eps.apexeps.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.eps.apexeps.models.relations.Ordena;
 import com.eps.apexeps.models.relations.OrdenaId;
 
+import java.util.Optional;
+
 @Repository
 public interface OrdenaRepository extends JpaRepository<Ordena, OrdenaId> {
+
+    @Query("""
+           SELECT o
+             FROM Ordena  o
+             JOIN o.id.agenda a
+            WHERE o.id.agenda.id    = :agendaId
+              AND o.id.servicio.cups = :cups
+              AND a.paciente.dni = :dniPaciente
+              AND a.estado <> 'CANCELADA'
+           """)
+    Optional<Ordena> validar(Integer agendaId, String cups, Long dniPaciente);
+
+
 }
