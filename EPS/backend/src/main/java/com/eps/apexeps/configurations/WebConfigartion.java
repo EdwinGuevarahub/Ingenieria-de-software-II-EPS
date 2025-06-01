9e6eb891eb1213d384f78d046a4b5e8edbb83e4d
Nicolás Sabogal <mew-four@hotmail.com>
2025-05-31 22:23:04 -0500

    feat: Configurar CORS para usar la URL del frontend desde variables de entorno, mejorando la flexibilidad de la configuración.

--- EPS/backend/src/main/java/com/eps/apexeps/configurations/WebConfigartion.java
+++ EPS/backend/src/main/java/com/eps/apexeps/configurations/WebConfigartion.java
@@ -2,16 +2,23 @@ package com.eps.apexeps.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
 
 /**
   * Clase de configuración para habilitar CORS en la aplicación.
   * @author Danna Villanueva
   */
 @Configuration
+@RequiredArgsConstructor
 public class WebConfigartion {
+
+    /** Permite acceder a las variables de application.properties */
+    private final Environment environment;
     
     /**
      * Configuración de CORS para permitir solicitudes desde el frontend.
@@ -23,7 +30,7 @@ public class WebConfigartion {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(environment.getProperty("frontend.url"))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
